dd919212e9265f8be2803c3d0a177be2be666f2f
Anup <anupmhrzn16@gmail.com>
2025-09-11 14:18:21 +0545

    feat:navigation scroll

--- website/src/components/common/Navigation.tsx
+++ website/src/components/common/Navigation.tsx
@@ -2,6 +2,7 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 type LinkType = {
   href: string;
@@ -16,19 +17,39 @@ interface NavigationProps {
 
 function Navigation({ links, theme = "light", className }: NavigationProps) {
   const pathname = usePathname();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsScrolled(scrollTop > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div
       className={cn(
-        "fixed z-50 w-full font-dosis px-[12rem] py-8 flex items-center justify-between tracking-widest text-lg",
+        "fixed z-50 w-full font-dosis px-[12rem] py-8 flex items-center justify-between tracking-widest text-lg transition-all border-transparent duration-300 ease-in-out",
         className,
-        theme === "light" ? "text-black" : "text-white"
+        theme === "light" ? "text-black" : "text-white",
+        isScrolled && [
+          theme === "light"
+            ? "bg-white/50 backdrop-blur-md shadow-lg border-b border-gray-200/50"
+            : "bg-black/50 backdrop-blur-md shadow-lg border-b border-white/10",
+          "py-4",
+        ]
       )}
     >
       <Link
         href="/"
         className={cn(
-          "header-nav !font-bold",
+          "header-nav !font-semibold transition-all duration-300",
           theme === "light" ? "nav-light" : "nav-dark",
           pathname === "/" && "active"
         )}
@@ -42,7 +63,7 @@ function Navigation({ links, theme = "light", className }: NavigationProps) {
             <Link
               href={link.href}
               className={cn(
-                "header-nav",
+                "header-nav transition-all duration-300",
                 theme === "light" ? "nav-light" : "nav-dark",
                 pathname === link.href && "active"
               )}
