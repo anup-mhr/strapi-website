ee865e43efd409fce24196373fa75762953839ac
RanjitXtha <shrestha.ranjit.np@gmail.com>
2025-09-11 10:08:17 +0545

    file changed

---
+++ website/src/constants/constants.ts
@@ -0,0 +1,14 @@
+export const links = [
+    { href: "/", label: "AKU ZELIANG" },
+    { href: "/studio", label: "URRA DESIGN STUDIO" },
+    { href: "/objects", label: "OBJECTS" },
+    { href: "/collaborations", label: "COLLABORATIONS" },
+    { href: "/events", label: "EVENTS" },
+    { href: "/contact", label: "CONTACT" },
+];
+
+export const contactPageLinks = [
+  { href: "/", label: "HOME" },
+  { href: "/studio", label: "URRA DESIGN STUDIO" },
+  { href: "/contact", label: "CONTACT" },
+];
