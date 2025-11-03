7e3bea9e32cfcd7ef9660c0a17864e52f66bb631
Anup <anupmhrzn16@gmail.com>
2025-11-03 17:47:45 +0545

    uodate

--- website/src/app/payment-modes/page.tsx
+++ website/src/app/payment-modes/page.tsx
@@ -21,7 +21,23 @@ async function page() {
         </h1>
 
         <div
-          className="mt-6 sm:mt-8 md:mt-10 tracking-wider **:font-inherit px-4 sm:px-0"
+          className="mt-6 sm:mt-8 md:mt-10 tracking-wider **:font-inherit px-4 sm:px-0
+           ck-content
+                    [&_p]:mb-4
+                    [&_h1]:text-3xl [&_h1]:font-semibold [&_h1]:mb-5
+                    [&_h2]:text-2xl [&_h2]:font-semibold [&_h2]:mb-4
+                    [&_h3]:text-xl [&_h3]:font-semibold [&_h3]:mb-3
+
+                    [&_ul]:list-disc [&_ul]:pl-6 [&_ul]:mb-4
+                    [&_ol]:list-decimal [&_ol]:pl-6 [&_ol]:mb-4
+                    [&_li]:mb-1
+
+                    [&_strong]:font-semibold
+                    [&_em]:italic
+
+                    [&_blockquote]:border-l-4 [&_blockquote]:border-gray-300 [&_blockquote]:pl-4 [&_blockquote]:italic [&_blockquote]:text-gray-600 [&_blockquote]:my-4
+
+                    [&_section]:mt-6 [&_section]:mb-6 "
           dangerouslySetInnerHTML={{ __html: paymentModes }}
         />
       </div>
