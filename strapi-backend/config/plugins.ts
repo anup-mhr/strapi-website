0dfc14f7398138a4b481ef266ea1e0116a8718da
Anup <anupmhrzn16@gmail.com>
2025-11-12 10:44:02 +0545

    feat:add email config

--- strapi-backend/config/plugins.ts
+++ strapi-backend/config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtpout.secureserver.net"),
+        port: env("SMTP_PORT", 465),
+        secure: true,
+        auth: {
+          user: env("SMTP_MAIL"),
+          pass: env("SMTP_PASS"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_MAIL"),
+        defaultReplyTo: env("SMTP_MAIL"),
+        testAddress: env("SMTP_MAIL"),
+      },
+    },
+  },
+});
